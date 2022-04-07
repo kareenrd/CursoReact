f1a12162ac354d7a6bd96cc25f96b46f7e50f49f
kareenrd <karenrodx@gmail.com>
2022-04-06 21:47:25 -0500

    Props

--- index.tsx
+++ index.tsx
@@ -4,7 +4,11 @@ import Hello from './Hello';
 import './style.css';
 
 function Hola(){
-  return <h1> holi! </h1>;
+  return <h1> holi! soy el titulo </h1>;
+}
+
+function Mostrar(props){
+  return <h1> holi! {props.nombre} </h1>;
 }
 
 interface AppProps { }
@@ -21,9 +25,11 @@ class App extends Component<AppProps, AppState> {
   }
 
   render() {
+    let nombre = "Karen";
     return (
       <div>
         <Hola />
+        <Mostrar nombre={nombre} />
       </div>
     );
   }
